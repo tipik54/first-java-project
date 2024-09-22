21033fd1e44649c0722a1653d6daaa31068eb6ae
Ruslan <suhorukovrus0@gmail.com>
2024-09-22 11:22:15 +0500

    homework1

--- MainApplication.java
+++ MainApplication.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainApplication {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in); //сканер чтобы пользователь выбирал какой метод вызвать и какие значения обретут переменные
+        Scanner scanner = new Scanner(System.in); //сканер для ввода пользователем значений
 
         while (true) {                      //Запуск цикла для проверки каждого задания не запуская программу с нуля
             System.out.println("Выберите операцию:");
