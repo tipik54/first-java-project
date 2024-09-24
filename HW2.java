05849c236103c6f7bc263c0cb38e8de06395c044
Ruslan <suhorukovrus0@gmail.com>
2024-09-24 13:38:14 +0500

    changed

--- HW2.java
+++ HW2.java
@@ -4,27 +4,18 @@ import java.util.Scanner;
 
 public class HW2 {
     public static void main(String[] args) {
-        int[] array = arrayData();
-        output(array);
+        numberOfPrints();
     }
 
-    public static int[] arrayData() {
+    public static void numberOfPrints() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество печатаний в консоль: ");
-        int arrayLength = scanner.nextInt();
-        int[] array = new int[arrayLength];
+        System.out.println("Введите количество печатаний в консоль");
+        int input = scanner.nextInt();
+        System.out.println("Введите что печатать");
+        String string = scanner.nextLine();
 
-        System.out.println("Введите что печатать: ");
-        int arrayData = scanner.nextInt();
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = arrayData;
-        }
-        return array;
-    }
-
-    public static void output(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        for (int i = 0; i < input; i++) {
+            System.out.println(string);
         }
     }
 }
