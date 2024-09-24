0ea42c98d8a9d86d4244c082792d90ae64ea2393
Ruslan <suhorukovrus0@gmail.com>
2024-09-24 13:51:39 +0500

    con

--- HW2.java
+++ HW2.java
@@ -1,5 +1,6 @@
 package ru.kaznu.ruslan.second.hw;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW2 {
@@ -7,6 +8,30 @@ public class HW2 {
 // Реализуйте метод, принимающий в качестве аргументов
 // целое число и ссылку на целочисленный массив,
 // увеличивающий каждый элемент которого на указанное число.
+        int[] array = arrayData();
+        increaseByNumber(array);
+
+    }
+
+    public static int[] arrayData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int arrayLength = scanner.nextInt();
+        System.out.println("Массив следующей длины: " + arrayLength);
+        int[] array = new int[arrayLength];
+        System.out.println("и имеет вид " + Arrays.toString(array));
+
+        System.out.println("Введите данные массива: ");
+        for (int i = 0; i < arrayLength; i++) {
+            int arrayData = scanner.nextInt();
+            array[i] = arrayData;
+            System.out.println("Массив имеет вид " + Arrays.toString(array));
+        }
+        System.out.println("Массив заполнен и имеет следующий вид " + Arrays.toString(array));
+        return array;
+    }
+
+    public static void increaseByNumber (int[] array) {
 
     }
 }
